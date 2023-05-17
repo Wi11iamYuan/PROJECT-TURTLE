7c4b5af6cfe7ed502d7cb874b98cae51e8e43f99
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-05-17 16:46:14 -0700

    changed to mirror progress on repl

--- turtle/src/App.js
+++ turtle/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Box({ text }) {
+  return (
+    <div className="box">
+      {text}
+    </div>
+  );
+}
+
+const boxes = [
+  { id: 1, text: "" },
+  { id: 2, text: "" },
+  { id: 3, text: "" },
+  { id: 4, text: "" },
+  { id: 5, text: "" },
+  { id: 6, text: "" }
+];
+
 
 function App() {
+  const [index, setIndex] = useState(0);
+  function handleKeyPress(e) {
+    console.log("You pressed a key." + e.key)
+    setIndex(index => index + 1);
+    console.log(index);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //onKeyPess from ChatGPT
+    <div className="App" tabIndex={0} onKeyPress={(e) => { handleKeyPress(e); }}>
+      {boxes.map(boxes => (
+        <Box key={boxes.id} text={boxes.text} />
+      ))}
     </div>
   );
 }
