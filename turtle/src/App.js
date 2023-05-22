8f5c090f3f1209572ff4b3419c65c67e72c9aa58
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-05-21 18:06:48 -0700

    progress on logic

--- turtle/src/App.js
+++ turtle/src/App.js
@@ -2,44 +2,79 @@ import './App.css';
 import { useState } from 'react';
 
 function Box({ letter, color }) {
+  let style = {
+    display: 'flex',
+  width: '50px',
+  height: '50px',
+  backgroundColor: color,
+  border: '5px solid #000',
+  padding: '10px',
+  margin: '5px',
+  textAlign: 'center'
+  }
   return (
-    <div className="box" style={{backgroundColor: {color}}}>
+    <div className="box" style = {style}>
       {letter}
     </div>
   );
 }
 
-const board = [
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
-  [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}]
- ];
+function Row( {guess, status} ){
+  let letters = guess.split("");
+  return (
+<div className='row'>
+  <Box letter = {letters[0]} color = {status[0]}/>
+  <Box letter = {letters[1]} color = {status[1]}/>
+  <Box letter = {letters[2]} color = {status[2]}/>
+  <Box letter = {letters[3]} color = {status[3]}/>
+  <Box letter = {letters[4]} color = {status[4]}/>
+  <Box letter = {letters[5]} color = {status[5]}/>
+</div>
+  );
+}
 
+let guesses = [
+"",
+"",
+"",
+"",
+"",
+"",
+""
+];
+
+let statuses = [
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white'],
+['white', 'white','white','white','white','white']
+];
 
 function App() {
-  function handleKeyPress(e) {
-    console.log("You pressed a key." + e.key)
-  }
+  const [row, setRow] = useState(0);
+  const [guessTrack, setGuessTrack] = useState(guesses);
+  
+   function handleKeyPress(e) {
+    console.log("You pressed a key " + e.key);
+    let word = guessTrack[0]
+    word= word+e.key;
+    let guess = guessTrack;
+    guess[0] = word
+    setGuessTrack(guess)
+    }
   return (
     //onKeyPess from ChatGPT
     <div className="App" tabIndex={0} onKeyPress={(e) => { handleKeyPress(e); }}>
-      {board.map((board,idx) => 
-      {
-        return (
-          <div className = "row" key={idx}>
-            {
-              board.map((row,idx) => {
-                return <Box letter = {row.letter} color= {row.status} key = {idx}/>
-              })
-            }  
-          </div>
-        );
-      }
-      )}
+          <Row className = "row" guess = {guesses[0]} status = {statuses[0]}/>
+          <Row className = "row" guess = {guesses[1]} status = {statuses[1]}/>
+          <Row className = "row" guess = {guesses[2]} status = {statuses[2]}/>
+          <Row className = "row" guess = {guesses[3]} status = {statuses[3]}/>
+          <Row className = "row" guess = {guesses[4]} status = {statuses[4]}/>
+          <Row className = "row" guess = {guesses[5]} status = {statuses[5]}/>
+          <Row className = "row" guess = {guesses[6]} status = {statuses[6]}/>
     </div>
   );
 }
